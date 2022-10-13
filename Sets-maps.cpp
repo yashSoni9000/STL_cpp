a2b9e32150bc7cb16268615feece2ef15aa445e5
Sunil <sunilkumawat7717@gmail.com>
2022-10-13 14:19:06 +0530

    Traversing using begin() and end()

--- Sets-maps.cpp
+++ Sets-maps.cpp
@@ -84,3 +84,25 @@ int main()
    mp1.emplace("raj",2);
    
 }
+
+
+// Map Traversing using begin() and end()
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//    int arr[] = { 1, 1, 2, 1, 1, 3, 4, 3 };
+//    int n = sizeof(arr) / sizeof(arr[0]);
+//
+//    map<int, int> m;
+//    for (int i = 0; i < n; i++)
+//        m[arr[i]]++;
+//
+//    cout << " Element Frequency" << endl;
+//    for (auto i = m.begin(); i != m.end(); i++)
+//        cout << i->first << "      " << i->second
+//             << endl;
+//
+//    return 0;
